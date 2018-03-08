674416c1280d568ac4be56919a466fdd068ec4d1
crmgunter <crmgunter@gmail.com>
2018-03-08 12:03:40 -0500

    localhost up and running, index page is working, fuck yeah

--- server.js
+++ server.js
@@ -6,3 +6,28 @@ const logger = require('morgan')
 
 const app = express()
 
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection.on('error', (err) => {
+    console.log(err)
+})
+
+//
+db.on('open', () => {
+    console.log('Connected to MongoDB... Rock \'n Roll')
+})
+
+// MIDDLEWARE
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+//set up routes
+app.get('/', (req, res) => {
+    res.send('Hello, Clarice. It\'s so nice to see you again')
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log('Meat sizzlin hot on PORT 3001.')
+})
