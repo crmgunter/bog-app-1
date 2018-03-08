4b535f12b710dc4e13bae63c200ea2ddf8bbc317
crmgunter <crmgunter@gmail.com>
2018-03-08 16:22:12 -0500

    all routes set up and working with postman

--- controllers/creatureController.js
+++ controllers/creatureController.js
@@ -3,8 +3,61 @@ const router = express.Router()
 const Creature = require('../models/CreatureModel')
 
 
-router.get('/', (req, res) => {
-    res.send('hey from creatures')
+router.get('/', async (req, res) => {
+    try {
+        const creatures = await Creature.find({})
+        res.json(creatures)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const creatureId = req.params.id
+        const creature = await Creature.findById(creatureId)
+        res.json(creature)
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newCreature = req.body
+        const savedCreature = await Creature.create(newCreature)
+        res.json(savedCreature)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const creatureId = req.params.id
+        const updatedCreature = req.body
+        const savedCreature = await Creature.findByIdAndUpdate(creatureId, updatedCreature)
+        res.json(savedCreature)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const creatureId = req.params.id
+        await Creature.findByIdAndRemove(creatureId)
+        res.json({
+            msg: 'Successfully Deleted'
+        })
+    } catch(err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
